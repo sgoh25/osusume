af40b8a18e5b5d5fe5cb1a03b339b0e5028dd377
Seraphine Goh <seraphinegoh@gmail.com>
2023-04-04 16:38:02 -0700

    Add initial post layout

---
+++ src/components/PostLayout.jsx
@@ -0,0 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import '../styles/Post.css'
+import Layout from './Layout.jsx'
+
+export default function PostLayout({ isCreate }) {
+    const navigate = useNavigate();
+
+    let body = (
+        <>
+            <div className="login_wrapper">
+                {isCreate && <div className="title">Create New Post</div>}
+                {!isCreate && <div className="title">Edit Post</div>}
+                <div className="label">Title:</div>
+                <input type="text" placeholder="Title"></input>
+                <div className="label">Body:</div>
+                <input type="text" placeholder="Body"></input>
+                <div className="login_button">
+                    <button className="button">Submit</button>
+                    <button className="button" onClick={() => navigate('/', { replace: true })}>Cancel</button>
+                </div>
+            </div>
+        </>
+    )
+
+    return <Layout buttons={<></>} body={body} />
+}
